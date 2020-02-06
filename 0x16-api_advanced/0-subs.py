ea426a1984772e15304a0af3cc3c5e7c40fda22c
dany <danyramirezg@gmail.com>
2020-02-06 16:11:52 -0500

    Queries the Reddit API and returns the number of subscribers. Fix pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers)
-for a given subreddit. If an invalid subreddit is given, the function should return 0"""
+"""function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0)"}
+    header = \
+        {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0)"}
     resp = requests.get(url, headers=header)
 
     if resp.status_code == 200:
