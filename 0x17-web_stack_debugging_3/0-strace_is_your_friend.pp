ee7c89e93fa684881610aa2cef2cbc18a768fcf8
Dany <danyramirezg@gmail.com>
2020-02-19 13:54:02 -0500

    Fix phpp to php. Correct output

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Correct error from .phpp to php
-exec { 'fix error':
- command  => 'sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php && sudo service apache2 restart',
+exec { 'Fix error':
+  command  => 'sed -i \'s/.phpp/.php/\' /var/www/html/wp-settings.php && sudo service apache2 restart',
   provider => shell,
 }
