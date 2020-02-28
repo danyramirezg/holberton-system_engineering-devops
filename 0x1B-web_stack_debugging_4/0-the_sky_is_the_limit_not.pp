0049896e2b60efba469b268d2f6b583bfbd9699d
danyramirezg <42430028+danyramirezg@users.noreply.github.com>
2020-02-28 16:49:29 -0500

    Update 0-the_sky_is_the_limit_not.pp
    Increase the requests' number to the server

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # Increase the requests' number to the server
 exec { 'Increase_request's_number':
-	command => '/bin/sed 'sed -i "s/15/20000/g" /etc/default/nginx | service nginx restart'
+	command => '/bin/sed -i "s/15/20000/g" /etc/default/nginx | service nginx restart'
 }
