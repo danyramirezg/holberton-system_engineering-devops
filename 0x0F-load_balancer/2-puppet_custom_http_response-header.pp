90afc6efdeb146b2ca93e88c37b047769ff272ab
Dany <danyramirezg@gmail.com>
2020-08-14 09:51:56 -0500

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -19,7 +19,13 @@ file { '/var/www/html/index.html':
 file_line { 'custom HTTP header':
     ensure => 'present',
     path => '/etc/nginx/sites-available/default',
-    line => 'add_header X-Served-By $HOSTNAME;',
+    line => 'add_header X-Served-By "$HOSTNAME";',
     after => 'listen 80 default_server;',
-    require => Package['nginx'],
+ }
+
+file_line { 'Redirection':
+    ensure => 'present';
+    path => '/etc/nginx/sites-available/default',
+    after => 'server_name _;',
+    line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=FGdXnX25z6Q permanent;',
 }
